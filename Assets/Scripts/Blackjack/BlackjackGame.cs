6a4ab66f9820aef20fa380a0ccddd0d42c9d8414
Andrei Mataoanu <amatao1999@gmail.com>
2025-12-07 19:18:43 +0100

    Prepared build for TAGS.

--- Assets/Scripts/Blackjack/BlackjackGame.cs
+++ Assets/Scripts/Blackjack/BlackjackGame.cs
@@ -711,9 +711,21 @@ public class BlackjackGame : MonoBehaviour
                 chosenEvent.Apply(this);
                 eventPool.RemoveAt(randomIndex);
 
+                statusText.text = "Let's make it more interesting";
+
+                AudioManager.instance.Play("Laugh");
+
+                dealerSmile.SetActive(true);
+
+                yield return new WaitForSeconds(5.0f);
+
+                AudioManager.instance.Play("NewEvent");
+
                 statusText.text = $"New Event: {chosenEvent.eventName}";
 
-                yield return new WaitForSeconds(3.0f);
+                yield return new WaitForSeconds(6.0f);
+
+                dealerSmile.SetActive(false);
             }
         }
     }
