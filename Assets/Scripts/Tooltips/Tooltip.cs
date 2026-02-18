232b0af2bd5897ecfbb3a2d257e90859629322b2
Andrei Mataoanu <amatao1999@gmail.com>
2026-02-18 10:16:35 +0100

    Made a better tooltip system.

---
+++ Assets/Scripts/Tooltips/Tooltip.cs
@@ -0,0 +1,43 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tooltip : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI headerText;
+    [SerializeField] private TextMeshProUGUI contentText;
+    [SerializeField] private LayoutElement layoutElement;
+    [SerializeField] private RectTransform rectTransform;
+    [SerializeField] private int characterWrapLimit;
+
+    private void Update()
+    {
+        Vector2 position = Input.mousePosition;
+
+        float pivotX = position.x / Screen.width;
+        float pivotY = position.y / Screen.height;
+
+        rectTransform.pivot = new Vector2(pivotX, pivotY);
+        transform.position = position;
+    }
+
+    public void SetText(string content, string header = "")
+    {
+        if(string.IsNullOrEmpty(header))
+        {
+            headerText.gameObject.SetActive(false);
+        }
+        else
+        {
+            headerText.gameObject.SetActive(true);
+            headerText.text = header;
+        }
+
+        contentText.text = content;
+
+        int headerLength = string.IsNullOrEmpty(header) ? 0 : headerText.text.Length;
+        int contentLength = contentText.text.Length;
+
+        layoutElement.enabled = (headerLength > characterWrapLimit || contentLength > characterWrapLimit) ? true : false;
+    }
+}
