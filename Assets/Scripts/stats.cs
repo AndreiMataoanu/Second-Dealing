af27cbb2dd9f19e24754a8d42a566c1ba5ee6bb6
Andrei Mataoanu <amatao1999@gmail.com>
2026-08-07 10:05:31 +0200

    Fixed cash out buttons, fixed a softlock?

--- Assets/Scripts/stats.cs
+++ Assets/Scripts/stats.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using Utils;
 
-public class stats : MonoBehaviour
+public class Stats : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI moneyEarnedText;
     [SerializeField] private TextMeshProUGUI timesWon;
@@ -12,25 +12,28 @@ public class stats : MonoBehaviour
     [SerializeField] private Animator fadeInAnimator;
     [SerializeField] private float delay = 2f;
 
-    void Start()
+    private void Start()
     {
+        Time.timeScale = 1f;
+
         moneyEarnedText.text = "Money earned: "+ PlayerPrefs.GetInt("PreviousRunMoney").ToString();
         timesWon.text = "Rounds won: " + PlayerPrefs.GetInt("PreviousRunWins").ToString();
         timesLost.text = "Rounds lost: " + PlayerPrefs.GetInt("PreviousRunLoss").ToString();
-
     }
-
 
     public void GoBack()
     {
         StartCoroutine(fadeRoutine());
     }
+
     public IEnumerator fadeRoutine()
     {
         fadeInAnimator.SetTrigger("fadeInTrig");
+
         yield return StartCoroutine(GameUtils.WaitDelayOrInput(delay));
+
         SceneManager.LoadSceneAsync(1);
+
         yield return null;
-        
     }
 }
